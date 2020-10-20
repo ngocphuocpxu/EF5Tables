37d775faac67e3b02d859cb1e5e94a766aa81f06
ngocphuocpxu <70925960+ngocphuocpxu@users.noreply.github.com>
2020-10-20 22:15:10 +0700

    Entity Framework with Database

---
+++ TestDataBase/TestDataBase/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestDataBase.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Thoisu> Thoisus { get; set; }
+        public List<Thegioi> Thegiois { get; set; }
+        public List<Thethao> Thethaos { get; set; }
+        public List<Suckhoe> Suckhoes { get; set; }
+        public List<Dulich> Duliches { get; set; }
+    }
+}
